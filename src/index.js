a7a674ccd5873d2fb64ef4ec5ebf55b2f0a9b2bd
Andrew <ajestkov@gmail.com>
2016-11-12 02:40:52 +0500

    gogogo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+app.use(cors());
+
+app.get('/', function (req, res) {
+  var link = req.query.username;
+
+  if (link == undefined) {
+    res.status(200).send('Invalid username');
+    return;
+  }
+  var entities = link.split(' ');
+  if (entities.length != 1) {
+    res.status(200).send('Invalid username');
+    return;
+  }
+  var re = new RegExp('(https?)?:?(\/\/)?[a-z]*[\.a-z]*(\/)?([a-zA-Z0-9]+)');
+  var re1 = new RegExp('\/|\.');
+  if (!re1.test(link)) {
+    console.log("RE1 " + link);
+    res.status(200).send('@' + link);
+    return;
+  }
+  var result = link.match(re);
+  console.log(result);
+  if (result[4].length <= 1)
+    res.status(200).send('@' + result[0]);
+  else res.status(200).send('@' + result[4]);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000');
+});
